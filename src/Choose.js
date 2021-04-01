8c083e6c89bef6a4f5d326079d32625b9ae58230
mhendricks96 <mhendricks96@yahoo.com>
2021-04-01 13:05:30 -0700

    finished lab4

--- src/Choose.js
+++ src/Choose.js
@@ -10,11 +10,11 @@ class Choose extends React.Component {
         <Dropdown>
 
           <DropdownButton id="dropdown-item-button" title="Number of Horns" >
-            <Dropdown.Item as="button" onClick={this.props.showHorns} >1</Dropdown.Item>
-            <Dropdown.Item as="button" onClick={this.props.showHorns} >2</Dropdown.Item>
-            <Dropdown.Item as="button" onClick={this.props.showHorns} >3</Dropdown.Item>
-            <Dropdown.Item as="button" onClick={this.props.showHorns} >100</Dropdown.Item>
-            <Dropdown.Item as="button" onClick={this.props.showHorns} >All</Dropdown.Item>
+            <Dropdown.Item as="button" value = "1" onClick={this.props.showHorns} >1</Dropdown.Item>
+            <Dropdown.Item as="button" value = "2" onClick={this.props.showHorns} >2</Dropdown.Item>
+            <Dropdown.Item as="button" value = "3" onClick={this.props.showHorns} >3</Dropdown.Item>
+            <Dropdown.Item as="button" value = "100" onClick={this.props.showHorns} >More</Dropdown.Item>
+            <Dropdown.Item as="button" value = "all"onClick={this.props.showHorns} >All</Dropdown.Item>
           </DropdownButton>
 
         </Dropdown>
